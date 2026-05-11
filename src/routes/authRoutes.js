9e397ca9c6c619c6389f5a8b6ed3ab30735ccae7
seu nome de usuário <pealq63@gmail.com>
2026-05-11 15:14:08 -0300

    adicionado rate limiting nas rotas de cadastro e login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,34 @@
 import express from "express";
 import autenticar from "../middleware/authMiddleware.js";
+import rateLimit from "express-rate-limit"; //
 import {register, login, getAllUsers, putDadosUser, searchUser, getOneUser} from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+// Limiter para login: 10 tentativas a cada 15 minutos por IP
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // janela de 1 minutos
+  max: 5,                   // máximo de 5 requisições nessa janela
+  message: {
+    message: "Muitas tentativas de login. Tente novamente em 15 minutos."
+  },
+  standardHeaders: true,  // envia headers RateLimit-* na resposta
+  legacyHeaders: false,
+});
+
+// Limiter para cadastro: mais permissivo, 5 por hora
+const registerLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: {
+    message: "Limite de cadastros atingido. Tente novamente em 1 minuto."
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+router.post("/register", registerLimiter ,register);
+router.post("/login", loginLimiter ,login);
 
 router.get("/users", autenticar,getAllUsers);
 router.get("/buscar", autenticar, searchUser);
