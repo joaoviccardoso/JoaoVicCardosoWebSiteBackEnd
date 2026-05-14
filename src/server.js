db1154cf7ad74599186dc3bb6821f9d069c1ed1c
seu nome de usuário <pealq63@gmail.com>
2026-05-14 16:41:18 -0300

    configurado para fazer deploy na render

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import manipuladorDeErros from "./middleware/manipuladorDeErros.js";
 import manipulador404 from "./middleware/manipulador404.js";
-
+const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 const app = express();
@@ -20,8 +20,8 @@ app.use("/produtos", productRoutes);
 app.use(manipulador404)
 app.use(manipuladorDeErros);
 
-connectDB();
-
-app.listen(3000, () => {
-  console.log("Servidor rodando");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
 });
