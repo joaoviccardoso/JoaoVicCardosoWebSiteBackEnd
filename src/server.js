3b29afb41f92ec2fc3ed41517da9025701cbfbb8
seu nome de usuário <pealq63@gmail.com>
2026-05-20 15:28:11 -0300

    cors desenvolvimento e producao

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import manipuladorDeErros from "./middleware/manipuladorDeErros.js";
 import manipulador404 from "./middleware/manipulador404.js";
-const PORT = process.env.PORT || 3000;
+
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const allowedOrigins = [
@@ -17,29 +17,30 @@ const allowedOrigins = [
   "https://www.jvcode.tech"
 ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
 
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(null, false); // melhor que lançar erro
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-};
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/produtos", productRoutes);
 
-app.use(manipulador404)
+app.use(manipulador404);
 app.use(manipuladorDeErros);
 
 connectDB().then(() => {
